a48aeac053f06d07d8eeeb27c007ad67f4ae2453
Jason Harrington <61073321+JHarringtonDev@users.noreply.github.com>
2025-07-09 20:37:54 -0700

    Added movement and modifier to bullet

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -2,10 +2,10 @@ using UnityEngine;
 
 public class BulletScript : MonoBehaviour
 {
-
+    [SerializeField] int moveSpeed;
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position -= new Vector3(0, 0, moveSpeed) * Time.deltaTime;
     }
 }
